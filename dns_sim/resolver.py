6a8d89535073a19ca0ff4ae4e13b6101b2d56acc
Ivanovich Chiu <ivanovich.chiu@cetys.edu.mx>
2025-04-07 16:56:39 -0700

    get local IP address

--- dns_sim/resolver.py
+++ dns_sim/resolver.py
@@ -1,4 +1,10 @@
-dns_records = {"webcraft.local": "192.168.1.100"}
+import socket
+
+# Dynamically get the local IP address
+local_ip = socket.gethostbyname(socket.gethostname())
+
+# Update the DNS records with the local IP
+dns_records = {"webcraft.local": local_ip}
 
 def resolve(domain):
     return dns_records.get(domain, "Domain not found.")
